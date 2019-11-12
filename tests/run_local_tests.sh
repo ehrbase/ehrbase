96208c808782beff48c65cd4110afa4384941f6f
Wlad Wagner <38438046+testautomation@users.noreply.github.com>
2019-11-12 09:25:59 +0100

    [skip ci] removed copy/paste mistake

--- tests/run_local_tests.sh
+++ tests/run_local_tests.sh
@@ -142,4 +142,4 @@ rebot --outputdir results \
 # ## 2. Restart PostgreSQL DB container
 # docker run -e POSTGRES_USER=postgres \
 #            -e POSTGRES_PASSWORD=postgres \
-#            -d -p 5432:5432 rippleosi/ethercis-database-docker
+#            -d -p 5432:5432 ehrbaseorg/ehrbase-database-docker:11.5
