fbcabd48843241b68fde1b9e6237d7a1cadecae1
Stefan Spiska <s.spiska@symeda.de>
2019-11-15 13:58:18 +0100

    ehrbase/project_management#10 cleanup

--- service/src/main/java/org/ehrbase/dao/access/jooq/TemplateStoreAccess.java
+++ service/src/main/java/org/ehrbase/dao/access/jooq/TemplateStoreAccess.java
@@ -1,20 +1,20 @@
 /*
  *
- *  * Copyright (c) 2019 Stefan Spiska (Vitasystems GmbH) and Hannover Medical School.
- *  *
- *  * This file is part of project EHRbase
- *  *
- *  * Licensed under the Apache License, Version 2.0 (the "License");
- *  * you may not use this file except in compliance with the License.
- *  * You may obtain a copy of the License at
- *  *
- *  * http://www.apache.org/licenses/LICENSE-2.0
- *  *
- *  * Unless required by applicable law or agreed to in writing, software
- *  * distributed under the License is distributed on an "AS IS" BASIS,
- *  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  * See the License for the specific language governing permissions and
- *  * limitations under the License.
+ *   Copyright (c) 2019 Stefan Spiska (Vitasystems GmbH) and Hannover Medical School.
+ *
+ *   This file is part of project EHRbase
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
  *
  */
 
@@ -89,7 +89,7 @@ public class TemplateStoreAccess extends DataAccess implements I_TemplateStoreAc
         if (force || templateStoreRecord.changed()) {
 
             if (!templateStoreRecord.changed()) {
-                //hack: force tell jOOQ to perform updateComposition whatever...
+                //hack: force tell jOOQ to perform update whatever...
                 templateStoreRecord.changed(true);
             }
             templateStoreRecord.setSysTransaction(transactionTime);
