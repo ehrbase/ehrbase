eee05bdc3e60e58b1d0062bbb0d8b6e0ca743c6f
chevalleyc <christianchevalley@gmail.com>
2020-01-30 14:59:28 +0700

    check mandatory elements

---
+++ base/src/main/resources/db/migration/V15__fix_plpgsql_js_functions.sql
@@ -0,0 +1,26 @@
+CREATE OR REPLACE FUNCTION ehr.js_dv_date_time(TIMESTAMP WITHOUT TIME ZONE, TEXT)
+  RETURNS JSON AS
+$$
+DECLARE
+  date_time ALIAS FOR $1;
+  time_zone ALIAS FOR $2;
+BEGIN
+
+  IF (date_time IS NULL)
+  THEN
+    RETURN NULL;
+  END IF;
+
+  IF (time_zone IS NULL)
+  THEN
+    time_zone := 'UTC';
+  END IF;
+
+  RETURN
+    json_build_object(
+        '_type', 'DV_DATE_TIME',
+        'value', ehr.iso_timestamp(date_time)||time_zone
+      );
+END
+$$
+  LANGUAGE plpgsql;
