e361d9f270130bf901668e0b5a31e967e3deacec
chevalleyc <christianchevalley@gmail.com>
2020-02-13 09:43:20 +0700

    validate participations. Allow null value/null null flavour in Elements

--- test-data/src/main/java/org/ehrbase/test_data/composition/CompositionTestDataCanonicalXML.java
+++ test-data/src/main/java/org/ehrbase/test_data/composition/CompositionTestDataCanonicalXML.java
@@ -33,7 +33,8 @@ public enum CompositionTestDataCanonicalXML {
     RIPPLE_COMFORMANCE_INSTRUCTION("Instruction section from Ripple comformance", "RIPPLE_conformanceTesting_INSTRUCTION.request-procedure.v1.xml"),
     REGISTRO_DE_ATENDIMENTO("Duplicate section headings with different name", "Registro_de_Atendimento_Clinico.xml"),
     //this canonical XML has the
-    ALL_TYPES_FIXED("test all node types without archetype details at the moment", "test_all_types.fixed.v1.xml");
+    ALL_TYPES_FIXED("test all node types without archetype details at the moment", "test_all_types.fixed.v1.xml"),
+    ALL_TYPES_INVALID_PARTICIPATIONS("simple test with participations holding a wrong value", "test_all_types_participations_invalid.xml");
 
 
     private final String filename;
