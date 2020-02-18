2bda0560d8b6d4683536981276536892cbaab623
Axel Siebert <a.siebert_84@gmx.de>
2020-02-18 20:22:45 +0100

    ehrbase/project_management#168 Add migration script to add unique constraint

---
+++ base/src/main/resources/db/migration/V17__make_directory_paths_unique.sql
@@ -0,0 +1,27 @@
+/*
+ * Modifications copyright (C) 2019 Vitasystems GmbH,  Hannover Medical School.
+
+ * This file is part of Project EHRbase
+
+ * Copyright (c) 2015 Christian Chevalley
+ * This file is part of Project Ethercis
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+-- Table ehr.folder_hierarchy
+
+-- Add constraint for unique parent-child pairs
+ALTER TABLE ehr.folder_hierarchy
+ADD CONSTRAINT UQ_FolderHierarchy_Parent_Child
+UNIQUE(parent_folder, child_folder);
