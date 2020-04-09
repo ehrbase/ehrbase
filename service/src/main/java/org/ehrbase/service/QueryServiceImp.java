26ef2336eb1abf67684125f42ba99bc3364b4629
chevalleyc <christianchevalley@gmail.com>
2020-04-09 13:11:47 +0700

    reverse commit

--- service/src/main/java/org/ehrbase/service/QueryServiceImp.java
+++ service/src/main/java/org/ehrbase/service/QueryServiceImp.java
@@ -109,13 +109,11 @@ public class QueryServiceImp extends BaseService implements QueryService {
         for (Record record : aqlResult.getRecords()) {
             Map<String, Object> fieldMap = new LinkedHashMap<>();
             for (Field field : record.fields()) {
-                //process non-hidden variables
-                if (aqlResult.getVariables().containsValue(field.getName())) {
-                    if (record.getValue(field) instanceof JsonElement) {
-                        fieldMap.put(field.getName(), new StructuredString((record.getValue(field)).toString(), StructuredStringFormat.JSON));
-                    } else
-                        fieldMap.put(field.getName(), record.getValue(field));
+                if (record.getValue(field) instanceof JsonElement){
+                    fieldMap.put(field.getName(), new StructuredString((record.getValue(field)).toString(), StructuredStringFormat.JSON));
                 }
+                else
+                    fieldMap.put(field.getName(), record.getValue(field));
             }
 
             resultList.add(fieldMap);
