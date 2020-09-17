24958a466a61cd4bbd9f563324803abaad3ed551
Stefan Spiska <s.spiska@symeda.de>
2020-09-17 12:21:28 +0200

    fix test
    see https://github.com/ehrbase/project_management/issues/367

--- service/src/test/java/org/ehrbase/dao/access/jooq/AqlQueryHandlerTest.java
+++ service/src/test/java/org/ehrbase/dao/access/jooq/AqlQueryHandlerTest.java
@@ -49,11 +49,9 @@ public class AqlQueryHandlerTest {
         AqlResult aqlResult = cut.process("select e/ehr_id/value from EHR e LIMIT 10 OFFSET 5");
         assertThat(aqlResult.getExplain().get(0)).hasSize(3).contains("10", "5");
         assertThat(aqlResult.getExplain().get(0).get(0).replaceAll("alias_\\d+", "")).isEqualToIgnoringWhitespace(
-                "select distinct on (\"/ehr_id/value\") \"\".\"/ehr_id/value\" from (" +
                         "select \"ehr_join\".\"id\" as \"/ehr_id/value\" " +
                         "from \"ehr\".\"entry\" right outer join \"ehr\".\"composition\" as \"composition_join\" on \"composition_join\".\"id\" = \"ehr\".\"entry\".\"composition_id\" " +
                         "right outer join \"ehr\".\"ehr\" as \"ehr_join\" on \"ehr_join\".\"id\" = \"composition_join\".\"ehr_id\" " +
-                        ") as \"\"" +
                         "limit ? offset ?");
     }
 }
