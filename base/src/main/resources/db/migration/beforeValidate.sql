d50191a944c0af1e4b3735980fc97bf44bb4ef6f
stefan.spiska <stefan.spiska@vitagroup.ag>
2022-03-29 15:29:27 +0200

    fix before validation

--- base/src/main/resources/db/migration/beforeValidate.sql
+++ base/src/main/resources/db/migration/beforeValidate.sql
@@ -18,11 +18,63 @@ BEGIN
                WHERE (version, checksum) = ('62', -307543225)
               ))
            THEN
-              UPDATE ehr.flyway_schema_history
-              SET checksum = 1440169380
-             WHERE (version, checksum) = ('62', -307543225);
-           END IF;
-        END IF;
+UPDATE ehr.flyway_schema_history
+SET checksum = 1440169380
+WHERE (version, checksum) = ('62', -307543225);
+END IF;
+END IF;
+END
+$$;
+
+DO
+$$
+BEGIN
+        IF
+(EXISTS
+            (SELECT 1
+             FROM information_schema.tables
+             WHERE table_schema = 'ehr'
+               AND table_name = 'flyway_schema_history'
+            ))
+        THEN
+           IF
+            (EXISTS
+              (SELECT 1
+               FROM ehr.flyway_schema_history
+               WHERE (version, checksum) = ('16', -2009117355)
+              ))
+           THEN
+UPDATE ehr.flyway_schema_history
+SET checksum = 1370938853
+WHERE (version, checksum) = ('16', -2009117355);
+END IF;
+END IF;
+END
+$$;
+
+DO
+$$
+BEGIN
+        IF
+(EXISTS
+            (SELECT 1
+             FROM information_schema.tables
+             WHERE table_schema = 'ehr'
+               AND table_name = 'flyway_schema_history'
+            ))
+        THEN
+           IF
+            (EXISTS
+              (SELECT 1
+               FROM ehr.flyway_schema_history
+               WHERE (version, checksum) = ('61', -1745413492)
+              ))
+           THEN
+UPDATE ehr.flyway_schema_history
+SET checksum = -55836684
+WHERE (version, checksum) = ('61', -1745413492);
+END IF;
+END IF;
 END
 $$;
 
