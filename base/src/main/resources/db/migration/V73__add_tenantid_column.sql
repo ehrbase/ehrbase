be3a39afb0c47ec2482cb39d6ef93363288bebe0
stefan.spiska <stefan.spiska@vitagroup.ag>
2022-11-10 12:23:34 +0100

    change namespace column generation

--- base/src/main/resources/db/migration/V73__add_tenantid_column.sql
+++ base/src/main/resources/db/migration/V73__add_tenantid_column.sql
@@ -38,85 +38,45 @@ INSERT INTO ehr.tenant (
     'default_tenant'
 );
 
--- disable all trigger
 
 -- add namespace column to all non system tables
 
-ALTER TABLE ehr.access ADD namespace TEXT;
-ALTER TABLE ehr.attestation ADD namespace TEXT;
-ALTER TABLE ehr.attestation_ref ADD namespace TEXT;
-ALTER TABLE ehr.attested_view ADD namespace TEXT;
-ALTER TABLE ehr.audit_details ADD namespace TEXT;
-ALTER TABLE ehr.compo_xref ADD namespace TEXT;
--- ALTER TABLE ehr.concept ADD namespace TEXT;
-ALTER TABLE ehr.contribution ADD namespace TEXT;
-ALTER TABLE ehr.folder ADD namespace TEXT;
-ALTER TABLE ehr.folder_hierarchy ADD namespace TEXT;
-ALTER TABLE ehr.folder_hierarchy_history ADD namespace TEXT;
-ALTER TABLE ehr.folder_history ADD namespace TEXT;
-ALTER TABLE ehr.folder_items ADD namespace TEXT;
-ALTER TABLE ehr.folder_items_history ADD namespace TEXT;
-ALTER TABLE ehr.heading ADD namespace TEXT;
-ALTER TABLE ehr.identifier ADD namespace TEXT;
--- ALTER TABLE ehr.language ADD namespace TEXT;
-ALTER TABLE ehr.object_ref ADD namespace TEXT;
-ALTER TABLE ehr.object_ref_history ADD namespace TEXT;
-ALTER TABLE ehr.participation ADD namespace TEXT;
-ALTER TABLE ehr.participation_history ADD namespace TEXT;
-ALTER TABLE ehr.party_identified ADD namespace TEXT;
-ALTER TABLE ehr.status ADD namespace TEXT;
-ALTER TABLE ehr.status_history ADD namespace TEXT;
-ALTER TABLE ehr.stored_query ADD namespace TEXT;
-ALTER TABLE ehr.template_store ADD namespace TEXT;
-ALTER TABLE ehr.terminology_provider ADD namespace TEXT;
-ALTER TABLE ehr.session_log ADD namespace TEXT;
-ALTER TABLE ehr.ehr ADD namespace TEXT;
-ALTER TABLE ehr.entry ADD namespace TEXT;
-ALTER TABLE ehr.entry_history ADD namespace TEXT;
-ALTER TABLE ehr.composition ADD namespace TEXT;
-ALTER TABLE ehr.composition_history ADD namespace TEXT;
-ALTER TABLE ehr.event_context ADD namespace TEXT;
-ALTER TABLE ehr.event_context_history ADD namespace TEXT;
+ALTER TABLE ehr.access ADD namespace TEXT default '1f332a66-0e57-11ed-861d-0242ac120002';
+ALTER TABLE ehr.attestation ADD namespace TEXT default '1f332a66-0e57-11ed-861d-0242ac120002';
+ALTER TABLE ehr.attestation_ref ADD namespace TEXT default '1f332a66-0e57-11ed-861d-0242ac120002';
+ALTER TABLE ehr.attested_view ADD namespace TEXT default '1f332a66-0e57-11ed-861d-0242ac120002';
+ALTER TABLE ehr.audit_details ADD namespace TEXT default '1f332a66-0e57-11ed-861d-0242ac120002';
+ALTER TABLE ehr.compo_xref ADD namespace TEXT default '1f332a66-0e57-11ed-861d-0242ac120002';
+-- ALTER TABLE ehr.concept ADD namespace TEXT default '1f332a66-0e57-11ed-861d-0242ac120002';
+ALTER TABLE ehr.contribution ADD namespace TEXT default '1f332a66-0e57-11ed-861d-0242ac120002';
+ALTER TABLE ehr.folder ADD namespace TEXT default '1f332a66-0e57-11ed-861d-0242ac120002';
+ALTER TABLE ehr.folder_hierarchy ADD namespace TEXT default '1f332a66-0e57-11ed-861d-0242ac120002';
+ALTER TABLE ehr.folder_hierarchy_history ADD namespace TEXT default '1f332a66-0e57-11ed-861d-0242ac120002';
+ALTER TABLE ehr.folder_history ADD namespace TEXT default '1f332a66-0e57-11ed-861d-0242ac120002';
+ALTER TABLE ehr.folder_items ADD namespace TEXT default '1f332a66-0e57-11ed-861d-0242ac120002';
+ALTER TABLE ehr.folder_items_history ADD namespace TEXT default '1f332a66-0e57-11ed-861d-0242ac120002';
+ALTER TABLE ehr.heading ADD namespace TEXT default '1f332a66-0e57-11ed-861d-0242ac120002';
+ALTER TABLE ehr.identifier ADD namespace TEXT default '1f332a66-0e57-11ed-861d-0242ac120002';
+-- ALTER TABLE ehr.language ADD namespace TEXT default '1f332a66-0e57-11ed-861d-0242ac120002';
+ALTER TABLE ehr.object_ref ADD namespace TEXT default '1f332a66-0e57-11ed-861d-0242ac120002';
+ALTER TABLE ehr.object_ref_history ADD namespace TEXT default '1f332a66-0e57-11ed-861d-0242ac120002';
+ALTER TABLE ehr.participation ADD namespace TEXT default '1f332a66-0e57-11ed-861d-0242ac120002';
+ALTER TABLE ehr.participation_history ADD namespace TEXT default '1f332a66-0e57-11ed-861d-0242ac120002';
+ALTER TABLE ehr.party_identified ADD namespace TEXT default '1f332a66-0e57-11ed-861d-0242ac120002';
+ALTER TABLE ehr.status ADD namespace TEXT default '1f332a66-0e57-11ed-861d-0242ac120002';
+ALTER TABLE ehr.status_history ADD namespace TEXT default '1f332a66-0e57-11ed-861d-0242ac120002';
+ALTER TABLE ehr.stored_query ADD namespace TEXT default '1f332a66-0e57-11ed-861d-0242ac120002';
+ALTER TABLE ehr.template_store ADD namespace TEXT default '1f332a66-0e57-11ed-861d-0242ac120002';
+ALTER TABLE ehr.terminology_provider ADD namespace TEXT default '1f332a66-0e57-11ed-861d-0242ac120002';
+ALTER TABLE ehr.session_log ADD namespace TEXT default '1f332a66-0e57-11ed-861d-0242ac120002';
+ALTER TABLE ehr.ehr ADD namespace TEXT default '1f332a66-0e57-11ed-861d-0242ac120002';
+ALTER TABLE ehr.entry ADD namespace TEXT default '1f332a66-0e57-11ed-861d-0242ac120002';
+ALTER TABLE ehr.entry_history ADD namespace TEXT default '1f332a66-0e57-11ed-861d-0242ac120002';
+ALTER TABLE ehr.composition ADD namespace TEXT default '1f332a66-0e57-11ed-861d-0242ac120002';
+ALTER TABLE ehr.composition_history ADD namespace TEXT default '1f332a66-0e57-11ed-861d-0242ac120002';
+ALTER TABLE ehr.event_context ADD namespace TEXT default '1f332a66-0e57-11ed-861d-0242ac120002';
+ALTER TABLE ehr.event_context_history ADD namespace TEXT default '1f332a66-0e57-11ed-861d-0242ac120002';
 
--- insert dummy tenant
-
-UPDATE ehr.access SET namespace = '1f332a66-0e57-11ed-861d-0242ac120002' WHERE true;
-UPDATE ehr.attestation SET namespace = '1f332a66-0e57-11ed-861d-0242ac120002' WHERE true;
-UPDATE ehr.attestation_ref SET namespace = '1f332a66-0e57-11ed-861d-0242ac120002' WHERE true;
-UPDATE ehr.attested_view SET namespace = '1f332a66-0e57-11ed-861d-0242ac120002' WHERE true;
-UPDATE ehr.audit_details SET namespace = '1f332a66-0e57-11ed-861d-0242ac120002' WHERE true;
-UPDATE ehr.compo_xref SET namespace = '1f332a66-0e57-11ed-861d-0242ac120002' WHERE true;
--- UPDATE ehr.concept SET namespace = '1f332a66-0e57-11ed-861d-0242ac120002' WHERE true;
-UPDATE ehr.contribution SET namespace = '1f332a66-0e57-11ed-861d-0242ac120002' WHERE true;
-UPDATE ehr.folder SET namespace = '1f332a66-0e57-11ed-861d-0242ac120002' WHERE true;
-UPDATE ehr.folder_hierarchy SET namespace = '1f332a66-0e57-11ed-861d-0242ac120002' WHERE true;
-UPDATE ehr.folder_hierarchy_history SET namespace = '1f332a66-0e57-11ed-861d-0242ac120002' WHERE true;
-UPDATE ehr.folder_history SET namespace = '1f332a66-0e57-11ed-861d-0242ac120002' WHERE true;
-UPDATE ehr.folder_items SET namespace = '1f332a66-0e57-11ed-861d-0242ac120002' WHERE true;
-UPDATE ehr.folder_items_history SET namespace = '1f332a66-0e57-11ed-861d-0242ac120002' WHERE true;
-UPDATE ehr.heading SET namespace = '1f332a66-0e57-11ed-861d-0242ac120002' WHERE true;
-UPDATE ehr.identifier SET namespace = '1f332a66-0e57-11ed-861d-0242ac120002' WHERE true;
--- UPDATE ehr.language SET namespace = '1f332a66-0e57-11ed-861d-0242ac120002' WHERE true;
-UPDATE ehr.object_ref SET namespace = '1f332a66-0e57-11ed-861d-0242ac120002' WHERE true;
-UPDATE ehr.object_ref_history SET namespace = '1f332a66-0e57-11ed-861d-0242ac120002' WHERE true;
-UPDATE ehr.participation SET namespace = '1f332a66-0e57-11ed-861d-0242ac120002' WHERE true;
-UPDATE ehr.participation_history SET namespace = '1f332a66-0e57-11ed-861d-0242ac120002' WHERE true;
-UPDATE ehr.party_identified SET namespace = '1f332a66-0e57-11ed-861d-0242ac120002' WHERE true;
-UPDATE ehr.status SET namespace = '1f332a66-0e57-11ed-861d-0242ac120002' WHERE true;
-UPDATE ehr.status_history SET namespace = '1f332a66-0e57-11ed-861d-0242ac120002' WHERE true;
-UPDATE ehr.stored_query SET namespace = '1f332a66-0e57-11ed-861d-0242ac120002' WHERE true;
-UPDATE ehr.template_store SET namespace = '1f332a66-0e57-11ed-861d-0242ac120002' WHERE true;
-UPDATE ehr.terminology_provider SET namespace = '1f332a66-0e57-11ed-861d-0242ac120002' WHERE true;
-UPDATE ehr.session_log SET namespace = '1f332a66-0e57-11ed-861d-0242ac120002' WHERE true;
-UPDATE ehr.ehr SET namespace = '1f332a66-0e57-11ed-861d-0242ac120002' WHERE true;
-UPDATE ehr.entry SET namespace = '1f332a66-0e57-11ed-861d-0242ac120002' WHERE true;
-UPDATE ehr.entry_history SET namespace = '1f332a66-0e57-11ed-861d-0242ac120002' WHERE true;
-UPDATE ehr.composition SET namespace = '1f332a66-0e57-11ed-861d-0242ac120002' WHERE true;
-UPDATE ehr.composition_history SET namespace = '1f332a66-0e57-11ed-861d-0242ac120002' WHERE true;
-UPDATE ehr.event_context SET namespace = '1f332a66-0e57-11ed-861d-0242ac120002' WHERE true;
-UPDATE ehr.event_context_history SET namespace = '1f332a66-0e57-11ed-861d-0242ac120002' WHERE true;
-
--- enable all trigger
 
 -- change unique constraint on template_store
 
