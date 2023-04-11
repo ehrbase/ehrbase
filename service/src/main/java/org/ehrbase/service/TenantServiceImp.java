9194cabba7f4557a8d399bc86fddd8fb1a4fc108
Dmitry Tulba <tulbad1@gmail.com>
2023-04-11 12:40:25 +0300

    spotless

--- service/src/main/java/org/ehrbase/service/TenantServiceImp.java
+++ service/src/main/java/org/ehrbase/service/TenantServiceImp.java
@@ -19,7 +19,6 @@ package org.ehrbase.service;
 
 import java.util.List;
 import java.util.Optional;
-import java.util.function.Function;
 import java.util.stream.Collectors;
 import org.ehrbase.api.definitions.ServerConfig;
 import org.ehrbase.api.service.TenantService;
