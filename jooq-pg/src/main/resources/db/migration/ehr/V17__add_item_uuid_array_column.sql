1cd54c86ba5dcb3993b9c30252411f88dcef5500
Holger Reise <holger.reise@vitagroup.ag>
2024-11-20 10:06:34 +0100

    CDR-1552 simplified migration script

--- jooq-pg/src/main/resources/db/migration/ehr/V17__add_item_uuid_array_column.sql
+++ jooq-pg/src/main/resources/db/migration/ehr/V17__add_item_uuid_array_column.sql
@@ -19,49 +19,17 @@
 ALTER TABLE ehr_folder_data ADD IF NOT EXISTS item_uuids uuid[] NULL DEFAULT NULL;
 ALTER TABLE ehr_folder_data_history ADD IF NOT EXISTS item_uuids uuid[] NULL DEFAULT NULL;
 
-CREATE INDEX IF NOT EXISTS mig_ehr_folder_data_item_uuids ON ehr_folder_data (ehr_id) WHERE item_uuids IS NULL;
-CREATE INDEX IF NOT EXISTS mig_ehr_folder_data_history_item_uuids ON ehr_folder_data_history (ehr_id) WHERE item_uuids IS NULL;
+UPDATE ehr_folder_data
+SET
+    item_uuids = array(select (jsonb_array_elements(data -> 'i') -> 'x' ->> 'V')::uuid),
+    data = data - 'i'
+WHERE item_uuids is null;
 
-CREATE OR REPLACE PROCEDURE pg_temp.mig_folder_items(
-    rel_name regclass,
-    batch_size integer
-) LANGUAGE plpgsql
-AS $procedure$
-DECLARE
-    update_count integer := -1;
-    mig_start_time timestamp := clock_timestamp();
-    mig_time timestamp;
-BEGIN
-    RAISE NOTICE 'Start migration for % at %', rel_name, to_char(mig_start_time, 'HH24:MI:SS:MS');
-    
-    WHILE update_count  <> 0 LOOP
-        mig_time = clock_timestamp();
-
-        EXECUTE format($$
-        UPDATE %1$I
-        SET item_uuids = array(select (jsonb_array_elements(data -> 'i') -> 'x' ->> 'V')::uuid),
-        data = data - 'i'
-        WHERE item_uuids is null and ehr_id IN (SELECT ehr_id FROM %1$I WHERE item_uuids is null LIMIT $1);
-        $$, rel_name::text)
-        USING batch_size;
-
-        GET DIAGNOSTICS update_count := ROW_COUNT;
-
-        IF update_count = 0 THEN
-            RAISE NOTICE 'Finished migration for % in %', rel_name, to_char(clock_timestamp() - mig_start_time, 'HH24:MI:SS:MS');
-        ELSE
-            COMMIT;
-            RAISE NOTICE '[%] updated % in %', rel_name, update_count, to_char(clock_timestamp() - mig_time, 'HH24:MI:SS:MS');
-        END IF;
-    END LOOP;
-END;
-$procedure$;
-
-CALL pg_temp.mig_folder_items('ehr_folder_data'::regclass, 10000);
-CALL pg_temp.mig_folder_items('ehr_folder_data_history'::regclass, 1000);
+UPDATE ehr_folder_data_history
+SET
+    item_uuids = array(select (jsonb_array_elements(data -> 'i') -> 'x' ->> 'V')::uuid),
+    data = data - 'i'
+WHERE item_uuids is null;
 
 ALTER TABLE ehr_folder_data_history ALTER item_uuids DROP DEFAULT, ALTER item_uuids SET NOT NULL;
 ALTER TABLE ehr_folder_data ALTER item_uuids DROP DEFAULT, ALTER item_uuids SET NOT NULL;
-
-DROP INDEX mig_ehr_folder_data_item_uuids;
-DROP INDEX mig_ehr_folder_data_history_item_uuids;
