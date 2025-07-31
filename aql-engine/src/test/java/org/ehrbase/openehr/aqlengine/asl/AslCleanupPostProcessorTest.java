58bf8c4e55702670845fe475baadb04a3fc3b5f2
Razvan Grecu <razvan.grecu@fxbits.io>
2025-07-31 16:26:11 +0300

    CDR-2006 - Update CONTAINS semantics
    - Update the feature check (FromCheck) to limit at-code contains expressions usage.
    - Keep track of parent containment while traversing, in order to check for valid cases.
    - Update existing query used in a test for comparison to include the newer changes.

--- aql-engine/src/test/java/org/ehrbase/openehr/aqlengine/asl/AslCleanupPostProcessorTest.java
+++ aql-engine/src/test/java/org/ehrbase/openehr/aqlengine/asl/AslCleanupPostProcessorTest.java
@@ -194,7 +194,6 @@ class AslCleanupPostProcessorTest {
               SELECT
                 sOB_o_0.sOB_o_0_vo_id
                 sOB_o_0.sOB_o_0_num
-                sOB_o_0.sOB_o_0_num_cap
                 sOB_o_0.sOB_o_0_entity_name /* name/value */
               WHERE
                 sOB_o_0.?? -- COMPLEX ARCHETYPE_NODE_ID archetype_node_id EQ [AslRmTypeAndConcept[aliasedRmType=OB, concept=.test.v0]]
@@ -209,7 +208,7 @@ class AslCleanupPostProcessorTest {
             sE_el_0: StructureQuery
               SELECT
                 sE_el_0.sE_el_0_vo_id
-                sE_el_0.sE_el_0_num
+                sE_el_0.sE_el_0_citem_num
                 sE_el_0.sE_el_0_entity_name /* name/value */
                 sE_el_0.sE_el_0_data
               WHERE
@@ -223,10 +222,7 @@ class AslCleanupPostProcessorTest {
                       sOB_o_0.sOB_o_0_vo_id EQ sE_el_0.sE_el_0_vo_id
 
                   DelegatingJoinCondition ->
-                      sOB_o_0.sOB_o_0_num LT sE_el_0.sE_el_0_num
-
-                  DelegatingJoinCondition ->
-                      sOB_o_0.sOB_o_0_num_cap GT_EQ sE_el_0.sE_el_0_num
+                    sOB_o_0.sOB_o_0_num EQ sE_el_0.sE_el_0_citem_num
 
             p_eq_0: EncapsulatingQuery
               SELECT
