571a474cb22c412493b992eed09b19f1780aa41c
Vinzenz Müller <vinzenz.mueller@vitagroup.ag>
2026-08-03 16:21:15 +0200

    CDR-2025 extend DB script for FOLDER and EHR_STATUS

--- jooq-pg/src/main/resources/db/migration/ehr/V29__locatable_uid_indexing.sql
+++ jooq-pg/src/main/resources/db/migration/ehr/V29__locatable_uid_indexing.sql
@@ -15,18 +15,50 @@
  * See the License for the specific LANGUAGE governing permissions and
  * limitations under the License.
  */
-
+-- composition
 ALTER TABLE comp_data ADD COLUMN IF NOT EXISTS uid_root text;
 UPDATE comp_data
 SET uid_root = CASE
-    WHEN num = 0 THEN vo_id::text
-    ELSE split_part(data -> 'U' ->> 'V','::', 1)
+                   WHEN num = 0 THEN vo_id::text
+                   ELSE split_part(data -> 'U' ->> 'V','::', 1)
     END
 WHERE
     uid_root IS NULL
-    AND
+  AND
     (num = 0 OR
-      (entity_concept IS NULL AND data ? 'U'));
+     (entity_concept IS NULL AND data ? 'U'));
 
 --TODO index definition
 CREATE INDEX IF NOT EXISTS comp_data_uid_root_idx ON comp_data USING btree (uid_root);
+
+-- ehr_status
+ALTER TABLE ehr_status_data ADD COLUMN IF NOT EXISTS uid_root text;
+UPDATE ehr_status_data
+SET uid_root = CASE
+                   WHEN num = 0 THEN vo_id::text
+                   ELSE split_part(data -> 'U' ->> 'V','::', 1)
+    END
+WHERE
+    uid_root IS NULL
+  AND
+    (num = 0 OR
+     (entity_concept IS NULL AND data ? 'U'));
+
+--TODO index definition
+CREATE INDEX IF NOT EXISTS ehr_status_data_uid_root_idx ON ehr_status_data USING btree (uid_root);
+
+-- folder
+ALTER TABLE ehr_folder_data ADD COLUMN IF NOT EXISTS uid_root text;
+UPDATE ehr_folder_data
+SET uid_root = CASE
+                   WHEN num = 0 THEN vo_id::text
+                   ELSE split_part(data -> 'U' ->> 'V','::', 1)
+    END
+WHERE
+    uid_root IS NULL
+  AND
+    (num = 0 OR
+     (entity_concept IS NULL AND data ? 'U'));
+
+--TODO index definition
+CREATE INDEX IF NOT EXISTS ehr_folder_data_uid_root_idx ON ehr_folder_data USING btree (uid_root);
